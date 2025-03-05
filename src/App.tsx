a45e194e79480f426a86ba4e80ee9e8aa80d14f5
markbowen21 <mdbowen@usc.edu>
2025-03-05 10:37:11 -0800

    removed warning errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useAuthenticator } from '@aws-amplify/ui-react';
 //import type { Schema } from "../amplify/data/resource";
 //import { generateClient } from "aws-amplify/data";
